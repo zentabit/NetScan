18fc39a888b3b01aebf695ac5c064b06108b4533
zentabit <zentabit@gmail.com>
2017-01-01 18:59:31 +0100

    First upload

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net.NetworkInformation;
+using System.Net;
+using System.Diagnostics;
+
+namespace NetScan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ping sender = new Ping();
+
+            string ipBase = "192.168.1.";
+            Stopwatch s = new Stopwatch();
+            s.Start();
+
+            for(int i = 1; i < 256; i++)
+            {
+                try
+                {
+                    PingReply reply = sender.Send(ipBase + i.ToString(), 5);
+                    if (reply.Status == IPStatus.Success)
+                    {
+                        Console.WriteLine("Address: {0}", reply.Address);
+                        Console.WriteLine("Hostname: {0}", Dns.GetHostEntry(reply.Address).HostName);
+                        Console.WriteLine();
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Couldn't resolve host.");
+                    Console.WriteLine();
+                }
+                
+            }
+            s.Stop();
+            Console.WriteLine("Finished in {0}.", s.Elapsed);
+            Console.ReadKey();
+        }
+    }
+}
